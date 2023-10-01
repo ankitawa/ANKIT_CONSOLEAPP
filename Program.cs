3a60d58bb7075e80f1132583521489aa775b5b8e
ankitawa <71224102+ankitawa@users.noreply.github.com>
2023-10-01 22:22:23 +0530

    Update Program.cs
    ok

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Sagar Sir");
+            Console.WriteLine("Sagar Sir with ankit Verma");
         }
     }
 }
